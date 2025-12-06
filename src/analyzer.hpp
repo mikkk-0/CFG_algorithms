80bb2d3d3820eeca7667daf053dbd8e2a419637e
Mikhail Tyrin <tyrin.mp@phystech.edu>
2025-12-06 19:11:13 +0300

    Fix interface (was incorrect)

--- src/analyzer.hpp
+++ src/analyzer.hpp
@@ -1,6 +1,6 @@
 #include "grammar.hpp"
 
 struct Analyzer {
-    virtual void init(const Grammar&) = delete;
-    virtual bool proceed(const std::vector<term_t>&) = delete;
+    virtual void init(const Grammar&) = 0;
+    virtual bool proceed(const std::vector<term_t>&) = 0;
 };
